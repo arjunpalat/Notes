4b441b66de0ef83822c7a241698a3bf9e0a9d364
arjunpalat <palathinkaraarjun@gmail.com>
2024-01-14 01:31:33 +0530

    Add Request Logger

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const app = express();
 
 app.use(express.json());
 
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+app.use(requestLogger);
+
 let notes = [
   { id: 1, content: "HTML is easy", important: true },
   { id: 2, content: "Browser can execute only JavaScript", important: false },
@@ -26,7 +35,8 @@ app.get("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
   const note = notes.find((note) => note.id === id);
   if (note) response.json(note);
-  else response.status(404).end("Unable to find the requested note");
+  else
+    response.status(404).json({ Error: "Unable to find the requested note" });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -59,6 +69,11 @@ app.post("/api/notes", (request, response) => {
 
   response.json(note);
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ Error: "Unknown Endpoint" });
+};
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
