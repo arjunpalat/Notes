3817617c75639d1bb6fff6a435f8ad28224abbdf
arjunpalat <palathinkaraarjun@gmail.com>
2024-01-15 15:25:22 +0530

    Connect to MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const Note = require("./models/note");
 
 app.use(cors());
 app.use(express.static("dist"));
@@ -15,31 +17,18 @@ const requestLogger = (request, response, next) => {
 };
 app.use(requestLogger);
 
-let notes = [
-  { id: 1, content: "HTML is easy", important: true },
-  { id: 2, content: "Browser can execute only JavaScript", important: false },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
-
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
-
 app.get("/", (request, response) => {
   response.send("<h1>Welcome to Notes!</h1>");
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) response.json(note);
-  else
-    response.status(404).json({ Error: "Unable to find the requested note" });
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    /* toJSON will tranform our notes from DB to the following after converting to JSON
+    console.log(JSON.stringify(notes));
+    console.log(notes);
+    */
+    response.json(notes);
+  });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -49,8 +38,10 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end();
 });
 
-app.get("/api/notes", (request, response) => {
-  response.json(notes);
+app.get("/api/notes/:id", (request, response) => {
+  Note.findById(request.params.id).then((note) => {
+    response.json(note);
+  });
 });
 
 app.post("/api/notes", (request, response) => {
@@ -62,15 +53,14 @@ app.post("/api/notes", (request, response) => {
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: Boolean(body.important) || false,
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 const unknownEndpoint = (request, response) => {
@@ -78,7 +68,7 @@ const unknownEndpoint = (request, response) => {
 };
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
