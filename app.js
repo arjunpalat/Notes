9839fb9ebd0038605ad8ef68d6af0297e419187a
arjunpalat <palathinkaraarjun@gmail.com>
2024-01-23 22:04:55 +0530

    Introduce user administration and Mongo user model

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ require("express-async-errors");
 const app = express();
 const cors = require("cors");
 const notesRouter = require("./controllers/notes");
+const usersRouter = require("./controllers/users");
 const middleware = require("./utils/middleware"); // Custom middlewares that we have defined
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
@@ -29,6 +30,7 @@ app.use(express.json()); // To parse incoming JSON payload in request to a JSON 
 app.use(middleware.requestLogger);
 
 app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,4 +1,5 @@
 const Note = require("../models/note");
+const User = require("../models/user");
 
 const testNotes = [
   { content: "CSS enhances web design", important: true },
@@ -28,8 +29,14 @@ const notesInDB = async () => {
   explicit invocation of it by the custom object from Mongoose */
 };
 
+const usersInDB = async () => {
+  const users = await User.find({});
+  return users.map(user => user.toJSON());
+}
+
 module.exports = {
   testNotes,
   nonExistingID,
   notesInDB,
+  usersInDB
 };
